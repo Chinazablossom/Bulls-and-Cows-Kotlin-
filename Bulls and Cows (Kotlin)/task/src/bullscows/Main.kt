5cc723315005fc864b3ef4a6af2bf2c81ebecc30
chinazablossom <chinazablossom7@gmail.com>
2024-11-17 00:04:42 +0100

    stage 3

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -1,121 +1,77 @@
 package bullscows
 
-import java.lang.NumberFormatException
-import kotlin.random.Random
-import kotlin.random.nextUInt
-import kotlin.system.exitProcess
 
 
 fun main() {
-    BullsAndCows()
-}
 
-class BullsAndCows {
-
-    init {
-        println("Input the length of the secret code:")
-        val length = readIntInput()
-        println("Input the number of possible symbols in the code:")
-        val charterLength = readIntInput()
-        try {
-
-            validateInput(length, charterLength)
-
-            val secretCode = generateSecreteCode(length, charterLength)
-
-            println("Okay, let's start a game!")
-            var turn = 1
-            do {
-                var cows = 0
-                var bulls = 0
-
-                println("Turn $turn:")
-
-                val userInput = readln().trim()
-                (0..secretCode.lastIndex).forEach {
-                    val secretDigit = secretCode[it]
-                    val userDigit = userInput[it]
-
-                    if (secretCode.contains(userDigit)) {
-                        cows++
-                    }
-
-                    if (secretDigit.equals(userDigit, true)) {
-                        bulls++
-                        cows--
-                    }
-                }
-                turn++
-
-                if (cows == 0 && bulls == 0) println("Grade: None.")
-
-                if (cows >= 1 && bulls >= 1) println("Grade: ${if (bulls > 1) "$bulls bulls" else "$bulls bull"} and ${if (cows > 1) "$cows cows" else "$cows cow"}.")
-
-                if (cows >= 1 && bulls < 1) println("Grade: ${if (cows > 1) "$cows cows" else "$cows cow"}.")
-
-                if (cows < 1 && bulls >= 1) println("Grade: ${if (bulls > 1) "$bulls bulls" else "$bulls bull"}.${if (bulls == secretCode.length) "\nCongratulations! You guessed the secret code." else ""}")
+    val length = readln().toInt()
+   println(generateSecreteCode(length))
 
 
-            } while (bulls != secretCode.length)
-        } catch (e: StringIndexOutOfBoundsException) {
-            println("Error: Your input must be $length characters long ")
-        } catch (e: IllegalArgumentException) {
-            println("Error: ${e.message}")
-        }
 
-    }
+    /*  //a correctly guessed digit is a cow, and if its position is also correct, then it is a bull.
+
+      //val secretCode = (1000..9999).random().toString()
+      val secretCode = "9305"
+
+      val userInput = ""
 
 
-    private fun generateSecreteCode(length: Int, characterLength: Int): String {
-        val characters = ('0'..'9').toMutableList() + ('a'..'z').toMutableList()
-        val lastChar = if (characterLength > 10) 'a' + (characterLength - 11) else 'a'
-        val randomAlphabet = (characters[10]..lastChar).random()
+      var cows = 0
+      var bulls = 0
 
-        println("The secret is prepared: ${"*".repeat(length)} (0-9, a-${lastChar}).")
-        val uniqueDigits = mutableSetOf<Char>()
-        val randomNum = Random.nextUInt().toString() + randomAlphabet
+      (0..3).forEach {
+          val secretDigit = secretCode[it]
+          val userDigit = userInput[it]
 
-            randomNum.forEach {
-                if (uniqueDigits.size < length) {
-                    uniqueDigits.add(it)
-                    if ( uniqueDigits.size < length && !uniqueDigits.contains(randomAlphabet) ) {
-                        uniqueDigits.add(randomAlphabet)
-                    }
-                }
-            }
+          if (secretCode.contains(userDigit)) {
+              cows++
+          }
 
-        return uniqueDigits.shuffled().joinToString("")
-    }
+          if (secretDigit == userDigit ) {
+              bulls++
+              cows--
+          }
 
-    private fun readIntInput(): Int {
-        val input = readlnOrNull()?.trim()
-        try {
-            return input?.toInt() ?: throw NumberFormatException("Input must be a number.")
-        } catch (e: NumberFormatException) {
-            println("Error: \"$input\" isn't a valid number.")
-            exitProcess(0)
-        }
 
-    }
+      }
 
-    private fun validateInput(length: Int, charterLength: Int) {
 
-        if (length > 10) {
-            throw IllegalArgumentException("can't generate a secret number with a length of $length because there aren't enough unique digits.")
-        }
+      if (cows == 0 && bulls == 0) println("Grade: None. The secret code is $secretCode.")
 
-        if (charterLength !in 10..36) {
-            if (charterLength > 36) {
-                throw IllegalArgumentException("maximum number of possible symbols in the code is 36 (0-9, a-z).")
-            }
-            throw IllegalArgumentException("it's not possible to generate a code with a length of $length with $charterLength unique symbols.")
-        }
-    }
+      if (cows >= 1 && bulls >= 1) println("Grade: $bulls bull(s) and $cows cow(s). The secret code is $secretCode.")
+
+      if (cows >= 1 && bulls < 1) println("Grade: $cows cow(s). The secret code is $secretCode.")
+
+      if (cows < 1 && bulls >= 1) println("Grade: $bulls bull(s). The secret code is $secretCode.")
+  */
 
 
 }
 
 
+fun generateSecreteCode(length: Int): String {
+    if (length > 10 ) {
+        return "Error: can't generate a secret number with a length of $length because there aren't enough unique digits."
+    }
 
+    val pseudoRandomNumber: String = System.nanoTime().toString().reversed()
+    val uniqueDigits = mutableSetOf<Int>()
+    var firstDigitFound = false
 
+    while (uniqueDigits.size < length) {
+        pseudoRandomNumber.forEach {
+            if (uniqueDigits.size < length) {
+                if (!firstDigitFound && it != '0' && it.isDigit() ) {
+                    uniqueDigits.add(it.digitToInt())
+                    firstDigitFound = true
+                } else if (firstDigitFound && it.isDigit() && !uniqueDigits.contains(it.digitToInt())) {
+                    uniqueDigits.add(it.digitToInt())
+                }
+            }
+        }
+    }
+
+    return "The random secret number is ${uniqueDigits.joinToString("")}."
+}
 
