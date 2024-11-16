3ba9e674eaca5c12d297de44cfbcf967d2d44cbb
chinazablossom <chinazablossom7@gmail.com>
2024-11-17 00:09:33 +0100

    stage 6

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -1,21 +1,23 @@
 package bullscows
 
 import kotlin.random.Random
+import kotlin.random.nextUInt
 
 
 fun main() {
     //a correctly guessed digit is a cow, and if its position is also correct, then it is a bull.
-
     println("Please, enter the secret code's length:")
     val length = readln().toInt()
-    if (length > 10) {
-         println("Error: can't generate a secret number with a length of $length because there aren't enough unique digits.")
+    println("Input the number of possible symbols in the code:")
+    val charterLength =  readln().toInt()
+
+    if (length !in 1..36 ) {
+        println("Error: can't generate a secret number with a length of $length because there aren't enough unique digits.")
         return
     }
 
-    val secretCode = generateSecreteCode(length)
+    val secretCode = generateSecreteCode(length,charterLength)
     println("Okay, let's start a game!")
-
     var turn = 1
 
     do {
@@ -33,7 +35,7 @@ fun main() {
                     cows++
                 }
 
-                if (secretDigit == userDigit) {
+                if (secretDigit.equals(userDigit,true) ) {
                     bulls++
                     cows--
                 }
@@ -41,7 +43,7 @@ fun main() {
             turn++
 
         } catch (e: Exception) {
-            println("Invalid input Please enter a valid amount of numbers\n")
+            println("Invalid input Please enter a valid amount of numbers\n ${e.message}\n${e.cause}")
         }
 
 
@@ -53,30 +55,32 @@ fun main() {
 
         if (cows < 1 && bulls >= 1) println("Grade: ${if (bulls > 1) "$bulls bulls" else "$bulls bull"}.${if (bulls == secretCode.length) "\nCongratulations! You guessed the secret code." else ""}")
 
-
     } while (bulls != secretCode.length)
 
 }
 
 
-fun generateSecreteCode(length: Int): String {
-    val randomNum = Random.nextInt().toString()
-    val uniqueDigits = mutableSetOf<Int>()
-    var firstDigitFound = false
+
+fun generateSecreteCode(length: Int,characterLength: Int): String {
+    val characters = ('0'..'9').toMutableList() + ('a'..'z').toMutableList()
+    val lastChar = if (characterLength > 10) 'a' + (characterLength - 11) else 'a'
+    val randomAlphabet = (characters[10]..lastChar).random()
+
+    println("The secret is prepared: ${"*".repeat(length)} (0-9, a-${lastChar}).")
+    val uniqueDigits = mutableSetOf<Char>()
+    val randomNum = Random.nextUInt().toString() + randomAlphabet
 
     while (uniqueDigits.size < length) {
         randomNum.forEach {
             if (uniqueDigits.size < length) {
-                if (!firstDigitFound && it != '0' && it.isDigit()) {
-                    uniqueDigits.add(it.digitToInt())
-                    firstDigitFound = true
-                } else if (firstDigitFound && it.isDigit() && !uniqueDigits.contains(it.digitToInt())) {
-                    uniqueDigits.add(it.digitToInt())
+                    uniqueDigits.add(it)
+                 if (!uniqueDigits.contains(randomAlphabet) && uniqueDigits.size < length) {
+                    uniqueDigits.add(randomAlphabet)
                 }
             }
         }
     }
 
-    return uniqueDigits.joinToString("")
+    return uniqueDigits.shuffled().joinToString("")
 }
 
